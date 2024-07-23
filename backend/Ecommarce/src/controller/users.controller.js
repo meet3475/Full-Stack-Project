925ff81defedd1ecaae2f409f219621d462c6fa3
meet3475 <meetdobariya480@gmail.com>
2024-07-23 18:37:40 +0530

    done_23

--- backend/Ecommarce/src/controller/users.controller.js
+++ backend/Ecommarce/src/controller/users.controller.js
@@ -1,6 +1,6 @@
 const Users = require('../model/users.model');
 const bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 const craeteToken = async (id) => {
     try {
@@ -16,7 +16,7 @@ const craeteToken = async (id) => {
         console.log(user);
 
         const accessToken = await jwt.sign({
-            id: user._id,
+            _id: user._id,
             role: user.role,
             expiresIn: '1 hours'
         },
@@ -25,8 +25,7 @@ const craeteToken = async (id) => {
         )
 
         const refreshToken = await jwt.sign({
-            id : user._id,
-             expiresIn: '2 days'
+            _id: id,
         },
             'djkjkkj4679hbjjk',
             { expiresIn: '2 days' }
@@ -34,9 +33,9 @@ const craeteToken = async (id) => {
 
         user.refreshToken = refreshToken;
 
-        await user.save({validateBeforeSave : false})
-        
-        return {accessToken, refreshToken }
+        await user.save({ validateBeforeSave: false })
+
+        return { accessToken, refreshToken }
 
     } catch (error) {
         console.log(error.message);
@@ -89,7 +88,7 @@ const ragister = async (req, res) => {
         })
 
     } catch (error) {
-         res.status(500).json({
+        res.status(500).json({
             success: false,
             message: "internal server error" + error.message
         })
@@ -120,9 +119,9 @@ const login = async (req, res) => {
             })
         }
 
-        const {accessToken, refreshToken} = await craeteToken(user._id)
+        const { accessToken, refreshToken } = await craeteToken(user._id)
 
-        console.log({accessToken, refreshToken});
+        console.log({ accessToken, refreshToken });
 
         // res.status(200).json({
         //     success: true,
@@ -134,7 +133,7 @@ const login = async (req, res) => {
         const userDataF = await Users.findById({ _id: user._id }).select("-password -refreshToken")
 
         const option = {
-            httpOnly : true,
+            httpOnly: true,
             secure: true
         }
 
@@ -142,8 +141,8 @@ const login = async (req, res) => {
             .cookie("accessToken", accessToken, option)
             .cookie("refreshToken", refreshToken, option)
             .json({
-                success : true,
-                message : "Login Sucessfully",
+                success: true,
+                message: "Login Sucessfully",
                 data: {
                     ...userDataF.toObject(),
                     accessToken
@@ -159,17 +158,65 @@ const login = async (req, res) => {
     }
 }
 
-const generateNewTokens = async (req, body) => {
+const generateNewTokens = async (req, res) => {
     try {
-        const {accessToken} = req.cookie
-        const 
+        console.log(req.cookies.refreshToken);
+        
+        const VerifyToken = await jwt.verify(req.cookies.refreshToken,  'djkjkkj4679hbjjk');
+        console.log(VerifyToken);
 
+        if (!VerifyToken) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Token"
+            })
+        }
+
+        const user = await Users.findById(VerifyToken._id);
+        console.log(user);
+
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "User Not Define"
+            })
+        }
+
+        if (req.cookies.refreshToken != user.toObject().refreshToken) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Token"
+            })
+        }
+
+        const { accessToken, refreshToken } = await craeteToken(user._id)
+
+        console.log({ accessToken, refreshToken });
+
+        const option = {
+            httpOnly: true,
+            secure: true
+        }
+
+        res.status(200)
+            .cookie("accessToken", accessToken, option)
+            .cookie("refreshToken", refreshToken, option)
+            .json({
+                success: true,
+                message: "Refresh Token Sucessfully",
+                data: {
+                    accessToken
+                }
+            })
 
     } catch (error) {
-        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "internal server error" + error.message
+        })
     }
 }
- 
+
 module.exports = {
     ragister,
     login,
