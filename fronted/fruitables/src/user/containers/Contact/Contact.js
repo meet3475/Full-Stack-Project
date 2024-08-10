5b299ecf859e810582f55b2233503c6d2596af39
meet3475 <meetdobariya480@gmail.com>
2024-08-10 16:27:01 +0530

    done

--- fronted/fruitables/src/user/containers/Contact/Contact.js
+++ fronted/fruitables/src/user/containers/Contact/Contact.js
@@ -60,7 +60,7 @@ function Contact(props) {
             {/* Contact Start */}
             <div className="container-fluid contact py-5">
                 <div className="container py-5">
-                    <div className="p-5  rounded">
+                    <div className="p-5 bg-light rounded">
                         <div className="row g-4">
                             <div className="col-12">
                                 <div className="text-center mx-auto" style={{ maxWidth: 700 }}>
@@ -69,7 +69,7 @@ function Contact(props) {
                                 </div>
                             </div>
                             <div className="col-lg-12">
-                                <div className="h-100 rounded">
+                                <div className="h-100 rounded bg-white">
                                     <iframe className="rounded w-100" style={{ height: 400 }} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387191.33750346623!2d-73.97968099999999!3d40.6974881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1694259649153!5m2!1sen!2sbd" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
                                 </div>
                             </div>
